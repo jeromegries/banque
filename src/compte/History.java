6fc5abe7c96e2fd2d72251919cfa2ea281d8f6f7
jerome <jerome@jerome-Latitude-E6520>
2015-11-26 11:47:41 +0100

    création foontion pour compter nb de lignes du fichier csv pour les afficher

--- src/compte/History.java
+++ src/compte/History.java
@@ -18,29 +18,37 @@ public class History {
     
     Csv csv = new Csv();
     
-    /*
-    public  String affichehistory () throws Exception
+    
+    public  String[] affichehistory () throws Exception
 		{
                 int i=0;
-                String[] result;
+                
                 CSVReader reader = new CSVReader(new FileReader(View.url));
     		String [] nextLine;
                 
                 //compter le nombre de lignes dans le fichier csv;
                 while ((nextLine = reader.readNext()) != null){ i=i+1;}
+                //stocker les lignes du csv dans un tableau
+                String[] result = new String[i];
                 
+                int c=0;
                 while ((nextLine = reader.readNext()) != null)
-   			 	 {
-                   for(String valeur:nextLine)
+                    {
+                   for(String valeur:nextLine){
                    		System.out.print (valeur);
                                 //sauter une ligne
+                                
                                 System.out.println ();
-                                result[]
-                                 return valeur;
+                                result[c]=valeur;
+                                c++;
+                        }    
+                        //return result;        
     		    }
+                    return result;
+                }
+}
                        
  
-        }
-*/
-    
-}
+        
+
+
